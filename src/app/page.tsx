219513118e18d1c52ec0ca9c882024651afa58c7
boothaw <andersonwilkesbooth@gmail.com>
2026-03-28 16:48:04 -0400

    posts filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,19 +18,35 @@ async function getPosts(): Promise<Post[]> {
 const Home = async () => {
   const posts = await getPosts();
 
+  console.log(posts)
+
   return (
     <div className="flex flex-col flex-1 items-center">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
           <h1>awilkesbooth.com</h1>
+          <h2 className="font-bold mb-4">jobs</h2>
             <div className="posts">
-              {posts.map((post) => {
+              {posts.filter((post) => post.categories.includes(3)).map((post) => {
                 return (
-                  <Link href={`/blog/${post.id}`} className="post" key={post.id}>
-                    <h3>{post.title.rendered}</h3>
-                    <p
+                  <div className={`post post-${post.id} mb-4`} key={post.id}>
+                    <h3 className="font-bold">{post.title.rendered} - {post.categories[0]}</h3>
+                    <div
                     dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-                    ></p>
-                  </Link>
+                    ></div>
+                  </div>
+                );
+              })}
+            </div>
+            <h2 className="font-bold mb-4">projects</h2>
+            <div className="posts">
+              {posts.filter((post) => post.categories.includes(4)).map((post) => {
+                return (
+                  <div className={`post post-${post.id} mb-4`} key={post.id}>
+                    <h3 className="font-bold">{post.title.rendered} - {post.categories[0]}</h3>
+                    <div
+                    dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+                    ></div>
+                  </div>
                 );
               })}
             </div>
