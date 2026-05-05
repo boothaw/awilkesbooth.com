9ab5ee105694ed9815e8d88be859a024ff965972
boothaw <andersonwilkesbooth@gmail.com>
2026-05-05 17:50:25 -0400

    updated fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Unbounded, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { SunAnimation } from "./components/SunAnimation";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const unbounded = Unbounded({
+  variable: "--font-unbounded",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${unbounded.variable} ${spaceGrotesk.variable} h-full antialiased`}
     >
       <body className="body">
         <SunAnimation />
