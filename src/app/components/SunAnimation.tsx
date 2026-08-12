730506d0bbd9004c373920712a7db845c6ef8c79
boothaw <andersonwilkesbooth@gmail.com>
2026-08-12 16:40:35 -0400

    title card update

--- src/app/components/SunAnimation.tsx
+++ src/app/components/SunAnimation.tsx
@@ -27,6 +27,7 @@ export function SunAnimation() {
     tl.fromTo(sunEl, {
       scale: 2
     },{scale: 1.25, y: 0, duration: 2.5, ease: 'power2.out' });
+    tl.addLabel('moving', 2.5);
     tl.fromTo(sun,
       { boxShadow: "0px 0px 0px 0px #f5a623" },
       { boxShadow: "0px 0px 15px 2px #f5a623", duration: 1.5, ease: "power2.out" },
@@ -34,7 +35,7 @@ export function SunAnimation() {
     )
     tl.fromTo(sunEl,{
       scale: 1.25
-    }, { scale: 1.0, x: 0, duration: 2.5, ease: 'power2.inOut' });
+    }, { scale: 1.0, x: 0, duration: 2.5, ease: 'power2.inOut' }, 'moving');
 
     if (horizonEl) {
       tl.to(horizonEl, { opacity: 0, duration: .2, ease: 'power2.in' }, 0.5);
